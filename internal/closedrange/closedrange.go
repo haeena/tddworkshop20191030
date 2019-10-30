bed408ebc1b1474defb0094db1b9e44f3d799d08
Toshiaki Hatano <th.hatano@ntt.com>
2019-10-30 15:23:18 +0900

    Green for Includeclosedrange except for nil case

--- internal/closedrange/closedrange.go
+++ internal/closedrange/closedrange.go
@@ -23,3 +23,13 @@ func (c1 *Closedrange) Equal(c2 *Closedrange) bool {
 	}
 	return c1.lower == c2.lower && c1.upper == c2.upper
 }
+
+func (c1 *Closedrange) IncludeClosedrange(c2 *Closedrange) bool {
+	if c1.Equal(c2) {
+		return true
+	}
+	if c2.lower >= c1.lower && c2.upper <= c1.upper {
+		return true
+	}
+	return false
+}
