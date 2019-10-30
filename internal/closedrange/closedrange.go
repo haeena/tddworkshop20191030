7f5018bd30f9e32b900dc3ff71a299c9906a5803
Toshiaki Hatano <th.hatano@ntt.com>
2019-10-30 15:00:40 +0900

    Green TestEqual / TestNotEqual passed

--- internal/closedrange/closedrange.go
+++ internal/closedrange/closedrange.go
@@ -16,3 +16,7 @@ func NewClosedrange(lower int, upper int) *Closedrange {
 func (c *Closedrange) Include(number int) bool {
 	return c.lower <= number && number <= c.upper
 }
+
+func (c1 *Closedrange) Equal(c2 *Closedrange) bool {
+	return c1.lower == c2.lower && c1.upper == c2.upper
+}
