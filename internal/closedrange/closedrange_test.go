7da158459c807a49b3578da3fa16c390c02a6c98
Toshiaki Hatano <th.hatano@ntt.com>
2019-10-30 15:20:45 +0900

    Red for Includeclosedrange

--- internal/closedrange/closedrange_test.go
+++ internal/closedrange/closedrange_test.go
@@ -142,3 +142,105 @@ func TestNotEqualToNil(t *testing.T) {
 	// 検証
 	assert.Equal(t, result, false, "与えた閉区間がNilである")
 }
+
+func TestIncludeClosedrange1(t *testing.T) {
+	// 初期化
+	lower1 := 1
+	upper1 := 5
+	obj1 := NewClosedrange(lower1, upper1)
+
+	lower2 := 1
+	upper2 := 5
+	obj2 := NewClosedrange(lower2, upper2)
+
+	// 動作
+	result := obj1.IncludeClosedrange(obj2)
+
+	// 検証
+	assert.Equal(t, result, true, "与えた閉区間が閉区間に含まれる")
+}
+
+func TestIncludeClosedrange2(t *testing.T) {
+	// 初期化
+	lower1 := 1
+	upper1 := 5
+	obj1 := NewClosedrange(lower1, upper1)
+
+	lower2 := -1
+	upper2 := 0
+	obj2 := NewClosedrange(lower2, upper2)
+
+	// 動作
+	result := obj1.IncludeClosedrange(obj2)
+
+	// 検証
+	assert.Equal(t, result, false, "与えた閉区間が閉区間に含まれない")
+}
+
+func TestIncludeClosedrange3(t *testing.T) {
+	// 初期化
+	lower1 := 1
+	upper1 := 5
+	obj1 := NewClosedrange(lower1, upper1)
+
+	lower2 := 0
+	upper2 := 2
+	obj2 := NewClosedrange(lower2, upper2)
+
+	// 動作
+	result := obj1.IncludeClosedrange(obj2)
+
+	// 検証
+	assert.Equal(t, result, false, "与えた閉区間が閉区間に含まれない")
+}
+
+func TestIncludeClosedrange4(t *testing.T) {
+	// 初期化
+	lower1 := 1
+	upper1 := 5
+	obj1 := NewClosedrange(lower1, upper1)
+
+	lower2 := 2
+	upper2 := 3
+	obj2 := NewClosedrange(lower2, upper2)
+
+	// 動作
+	result := obj1.IncludeClosedrange(obj2)
+
+	// 検証
+	assert.Equal(t, result, true, "与えた閉区間が閉区間に含まれる")
+}
+
+func TestIncludeClosedrange5(t *testing.T) {
+	// 初期化
+	lower1 := 1
+	upper1 := 5
+	obj1 := NewClosedrange(lower1, upper1)
+
+	lower2 := 2
+	upper2 := 6
+	obj2 := NewClosedrange(lower2, upper2)
+
+	// 動作
+	result := obj1.IncludeClosedrange(obj2)
+
+	// 検証
+	assert.Equal(t, result, false, "与えた閉区間が閉区間に含まれない")
+}
+
+func TestIncludeClosedrange6(t *testing.T) {
+	// 初期化
+	lower1 := 1
+	upper1 := 5
+	obj1 := NewClosedrange(lower1, upper1)
+
+	lower2 := 6
+	upper2 := 7
+	obj2 := NewClosedrange(lower2, upper2)
+
+	// 動作
+	result := obj1.IncludeClosedrange(obj2)
+
+	// 検証
+	assert.Equal(t, result, false, "与えた閉区間が閉区間に含まれない")
+}
